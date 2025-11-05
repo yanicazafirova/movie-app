acc3c75d9f4ef1e9cd7eddf749ffad9d193a9fa7
Alex Georgiev <georgievalex114@gmail.com>
2025-11-05 12:16:53 +0200

    Create SearchBar in home screen.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,27 @@
-import { Text, View } from "react-native";
-import {Link} from 'expo-router';
+import {Text, View, Image, ScrollView} from "react-native";
+import {images} from '@/constants/images';
+import {Link, useRouter} from 'expo-router';
+import {icons} from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 export default function Index() {
-  return (
-    <View className={'flex-1 justify-center items-center'}
-    >
-      <Text className={'text-primary text-5xl'}>Welcome</Text>
-    </View>
-  );
+    const router = useRouter();
+
+    return (
+        <View className={'flex-1 bg-primary'}>
+            <Image source={images.bg} className={'absolute w-full z-0'}/>
+            <ScrollView className={'flex-1 px-5'} showsVerticalScrollIndicator={false} contentContainerStyle={{
+                minHeight: '100%', paddingBottom: 10
+            }}>
+                <Image source={icons.logo} className={'w-12 f-10 mt-20 mb-10 mx-auto'}/>
+
+                <View className={'flex-1 mt-5'}>
+                    <SearchBar
+                        onPress={() => router.push("/search")}
+                        placeholder={'Search for a movie'}
+                    />
+                </View>
+            </ScrollView>
+        </View>
+    );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import {View, Text, Image, TextInput} from "react-native";
+import {icons} from "@/constants/icons";
+
+interface Props {
+    placeholder: string;
+    onPress?: () => void;
+}
+
+const SearchBar = ({onPress, placeholder} : Props) => {
+    return (
+        <View className={'flex-row items-center bg-dark-200 rounded-full px-5 py-4'}>
+            <Image source={icons.search} className={'size-5'} resizeMode={'contain'} tintColor={'#ab8bff'}/>
+            <TextInput
+                onPress={onPress}
+                placeholder={placeholder}
+                value={''}
+                onChangeText={() => {}}
+                placeholderTextColor={'#ab8bff'}
+                className={'flex-1 ml-2 text-white'}
+            />
+        </View>
+    )
+}
+
+export default SearchBar;
